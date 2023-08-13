2e8be2881c83905fbbb04cd3a1dd95f0695a89ae
Sihyun <sihyun930222@gmail.com>
2023-08-13 14:05:15 +0900

    feat: auth api 연동

--- app/api/oauth/code/route.ts
+++ app/api/oauth/code/route.ts
@@ -7,6 +7,8 @@ export async function GET(request: Request) {
   const { access_token: kakaoAccessToken, refresh_token: kakaoRefreshToken } =
     await getKakaoToken(code!);
 
+  const kakaoAccountInfo = await getUserInfo(kakaoAccessToken);
+
   const { accessToken, refreshToken } = fakeTokenExchange(
     kakaoAccessToken,
     kakaoRefreshToken
@@ -34,11 +36,11 @@ export async function GET(request: Request) {
 }
 
 const getKakaoToken = async (code: string) => {
-  const { KAKAO_API } = process.env;
+  const { KAKAO_API_KEY } = process.env;
   const url = "https://kauth.kakao.com/oauth/token";
   const params = {
     grant_type: "authorization_code",
-    client_id: `${KAKAO_API}`,
+    client_id: `${KAKAO_API_KEY}`,
     redirect_uri: "http://localhost:3000/api/oauth/code",
     code,
   };
@@ -56,7 +58,25 @@ const getKakaoToken = async (code: string) => {
         "content-type": "application/x-www-form-urlencoded;charset=utf-8",
       },
     });
-    return response.json();
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    console.error(e as Error);
+  }
+};
+
+const getUserInfo = async (accessToken: string) => {
+  const url = "https://kapi.kakao.com/v2/user/me";
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "content-type": "application/x-www-form-urlencoded;charset=utf-8",
+      },
+    });
+    const data = await response.json();
+    return data;
   } catch (e) {
     console.error(e as Error);
   }
--- app/api/oauth/kakao/route.ts
+++ app/api/oauth/kakao/route.ts
@@ -1,10 +1,15 @@
 import { redirect } from "next/navigation";
+import { NextResponse } from "next/server";
 
 export async function GET() {
   const { KAKAO_API_KEY } = process.env;
   const REDIRECT_URI = "http://localhost:3000/api/oauth/code";
-
-  return redirect(
-    `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`
+  const response = NextResponse.redirect(
+    `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`,
+    { status: 302 }
   );
+
+  response.headers.set("Access-Control-Allow-Origin", "*");
+
+  return response;
 }
